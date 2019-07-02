ac02f67b356dd2b3d3a67d1e2762e28f7b1771f9
Alibh Deori <alibh619@gmail.com>
2019-07-02 09:08:03 +0530

    added new app main for product info

--- views.py
+++ views.py
@@ -7,6 +7,16 @@ class ContactUsView(FormView):
     form_class = forms.ContactForm
     success_url = "/"
 
+    # def contact_us(self, request):
+    #     if request.method == 'POST':
+    #         form = forms.ContactForm(request.POST)
+    #     if form.is_valid():
+    #         form.send_mail()
+    #         return HttpResponseRedirect('/')
+    #     else:
+    #         form = forms.ContactForm()
+    #     return render(request, 'contact_form.html', {'form': form})
+
     def form_valid(self, form):
         form.send_mail()
         return super().form_valid(form)
