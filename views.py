46f2d38c7bea79ab8ef1604f5467a0802398cd97
Alibh Deori <alibh619@gmail.com>
2019-07-02 08:35:40 +0530

    project added home and about page

---
+++ views.py
@@ -0,0 +1,12 @@
+from django.views.generic.edit import FormView
+import forms
+
+
+class ContactUsView(FormView):
+    template_name = "contact_form.html"
+    form_class = forms.ContactForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.send_mail()
+        return super().form_valid(form)
